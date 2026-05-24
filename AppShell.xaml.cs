2ce5f61a3525c1867a95d0f66d11d37578b23f3c
Lucas <lucas.flima_@outlook.com.br>
2026-05-23 23:28:01 -0300

    chore: cria estrutura inicial do app MAUI

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimedApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AnimedApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
